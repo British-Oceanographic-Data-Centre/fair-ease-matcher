ba21c016de9551baabaac618d2a165aa897a27e9
qtl <qtl@noc.ac.uk>
2025-10-08 16:20:48 +0100

    include router prefix

--- src/app/fastapi_app.py
+++ src/app/fastapi_app.py
@@ -488,5 +488,4 @@ if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8007, reload=True)
 
-# app.include_router(router, prefix="/api")    
-app.include_router(router)    
+app.include_router(router, prefix="/api")    
